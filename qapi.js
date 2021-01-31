71741094125ac64fb661f101d3714f62f29302fc
rckthu <78324623+rckthu@users.noreply.github.com>
2021-01-31 23:12:59 +0100

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"60172a3dbfd603001f71a9a0",
+				"60172a3dbfd603001f71a9a2"
+			]
+		},
+		"60172a3dbfd603001f71a9a0": {
+			"id": "60172a3dbfd603001f71a9a0",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"60172a3dbfd603001f71a9a2": {
+			"id": "60172a3dbfd603001f71a9a2",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
